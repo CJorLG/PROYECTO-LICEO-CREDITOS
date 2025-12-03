b9cb0e2f2722107ad858f8036f59d5e20966c4bf
monc_A2910 <edmoncayo1008@gamil.com>
2025-12-03 08:28:01 -0600

    excel_mayusculas

--- abrirexcel.py
+++ abrirexcel.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
-df = pd.read_excel("C:\\Users\\edgar\\Documents\\prueba excel.xlsx")
+df = pd.read_excel("C:\\Users\\edgar\\Documents\\prueba_excel.xlsx")
+
+# Convertir absolutamente todo a TEXTO
+df = df.astype(str)
+
+# Ahora convertir todo a MAYÚSCULAS
+df = df.apply(lambda col: col.str.upper())
+
+
 print(df)
 
